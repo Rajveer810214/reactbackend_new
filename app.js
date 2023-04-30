df3af1804e5019d951ea0fbfea8862a86ae7f6cf
Rajveer <rajveer810214@gmail.com>
2023-05-01 00:07:11 +0530

    commit all files

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const path=require("path")
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors());
+//nice
 app.use(express.static(path.join(__dirname+"/public")))
 //   });
 app.get('*', (req, res) => {
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-    mongoose.connect(process.env.DATABASE)
+    mongoose.connect('mongodb+srv://rajveer810214:XJvZX0soh0Tz86Fq@cluster0.3nxuodm.mongodb.net/test')
       .then(() => console.log('Connected to MongoDB'))
       .catch((error) => console.log(error));
 const newSchema=new mongoose.Schema({
